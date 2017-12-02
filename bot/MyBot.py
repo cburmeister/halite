27b4b5093e6d8e1e1d04540ca1528c875e2905a3
Corey Burmeister <burmeister.corey@gmail.com>
2017-12-02 12:24:00 -0800

    Add get_command_for_undocked_ship

--- bot/MyBot.py
+++ bot/MyBot.py
@@ -9,6 +9,45 @@ logging.info('Starting my Settler bot!')
 destination_by_ship_id = {}
 
 
+def get_command_for_undocked_ship(game_map, ship):
+    """Return the best command for the given undocked ship."""
+
+    # Get all of the planets and sort them by distance from the ship
+    planets = game_map.all_planets()
+    planets = sorted(
+        planets,
+        key=lambda x: x.calculate_distance_between(ship)
+    )
+
+    # Iterate over the planets with the goal of sending a ship to one
+    for planet in planets:
+
+        # Skip this planet if it's already owned
+        if planet.is_owned():
+            continue
+
+        # Skip this planet if there's already a ship heading its way
+        if planet in destination_by_ship_id.values():
+            continue
+
+        # Dock the ship at the planet if possible
+        if ship.can_dock(planet):
+            command = ship.dock(planet)
+            destination_by_ship_id.pop(ship.id, None)
+            return command
+
+        # Otherwise head towards it
+        command = ship.navigate(
+            ship.closest_point_to(planet),
+            game_map,
+            speed=int(hlt.constants.MAX_SPEED),
+            ignore_ships=True
+        )
+        if command:
+            destination_by_ship_id[ship.id] = planet
+            return command
+
+
 while True:
     game_map = game.update_map()
     command_queue = []
@@ -20,42 +59,8 @@ while True:
 
     # Loop over every undocked ship and try to give them something to do
     for ship in ships_by_docking_status.get(ship.DockingStatus.UNDOCKED, []):
-
-        # Get all of the planets and sort them by distance from the ship
-        planets = game_map.all_planets()
-        planets = sorted(
-            planets,
-            key=lambda x: x.calculate_distance_between(ship)
-        )
-
-        # Iterate over the planets with the goal of sending a ship to one
-        for planet in planets:
-
-            # Skip this planet if it's already owned
-            if planet.is_owned():
-                continue
-
-            # Skip this planet if there's already a ship heading its way
-            if planet in destination_by_ship_id.values():
-                continue
-
-            # Dock the ship at the planet if possible
-            if ship.can_dock(planet):
-                command_queue.append(ship.dock(planet))
-                destination_by_ship_id.pop(ship.id, None)
-
-            # Otherwise head towards it
-            else:
-                navigate_command = ship.navigate(
-                    ship.closest_point_to(planet),
-                    game_map,
-                    speed=int(hlt.constants.MAX_SPEED),
-                    ignore_ships=True
-                )
-                if navigate_command:
-                    command_queue.append(navigate_command)
-                    destination_by_ship_id[ship.id] = planet
-
-            break
+        command = get_command_for_undocked_ship(game_map, ship)
+        if command:
+            command_queue.append(command)
 
     game.send_command_queue(command_queue)
