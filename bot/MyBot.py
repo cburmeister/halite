c022ba6c6eef99f221705c409f4c53b9e110481f
Corey Burmeister <burmeister.corey@gmail.com>
2017-12-02 02:06:12 -0800

    Add some comments

--- bot/MyBot.py
+++ bot/MyBot.py
@@ -12,24 +12,35 @@ while True:
     game_map = game.update_map()
     command_queue = []
     for ship in game_map.get_me().all_ships():
+
+        # Skip this ship if it's not undocked
         if ship.docking_status != ship.DockingStatus.UNDOCKED:
             continue
 
+        # Get all of the planets and sort them by distance from the ship
         planets = game_map.all_planets()
         planets = sorted(
             planets,
             key=lambda x: x.calculate_distance_between(ship)
         )
+
+        # Iterate over the planets with the goal of sending a ship to one
         for planet in planets:
+
+            # Skip this planet if it's already owned
             if planet.is_owned():
                 continue
 
+            # Skip this planet if there's already a ship heading its way
             if planet in destination_by_ship_id.values():
                 continue
 
+            # Dock the ship at the planet if possible
             if ship.can_dock(planet):
                 command_queue.append(ship.dock(planet))
                 destination_by_ship_id.pop(ship.id, None)
+
+            # Otherwise head towards it
             else:
                 navigate_command = ship.navigate(
                     ship.closest_point_to(planet),
@@ -40,6 +51,7 @@ while True:
                 if navigate_command:
                     command_queue.append(navigate_command)
                     destination_by_ship_id[ship.id] = planet
+
             break
 
     game.send_command_queue(command_queue)
