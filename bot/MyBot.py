979e0398e6be7558dd0680e37a28c74d245e1b00
Corey Burmeister <burmeister.corey@gmail.com>
2017-12-02 00:08:05 -0800

    Don't send ships to the same place at the same time

--- bot/MyBot.py
+++ bot/MyBot.py
@@ -5,6 +5,8 @@ import hlt
 game = hlt.Game('Settler')
 logging.info('Starting my Settler bot!')
 
+destination_by_ship_id = {}
+
 while True:
     game_map = game.update_map()
     command_queue = []
@@ -16,8 +18,12 @@ while True:
             if planet.is_owned():
                 continue
 
+            if planet in destination_by_ship_id.values():
+                continue
+
             if ship.can_dock(planet):
                 command_queue.append(ship.dock(planet))
+                destination_by_ship_id.pop(ship.id, None)
             else:
                 navigate_command = ship.navigate(
                     ship.closest_point_to(planet),
@@ -27,6 +33,7 @@ while True:
                 )
                 if navigate_command:
                     command_queue.append(navigate_command)
+                    destination_by_ship_id[ship.id] = planet
             break
 
     game.send_command_queue(command_queue)
